e4044e944c40d7f7126f84c18211ca2e014fa2dd
Krzysztof Szyper <krzysztof_szyper@epam.com>
2019-11-22 13:56:28 +0100

    Verbose for format-hcl.sh

--- fmt/format-hcl.sh
+++ fmt/format-hcl.sh
@@ -4,5 +4,8 @@
 # Runs `terraform fmt` for all .tf files
 # Run `/terragrunt-fmt.sh` for all .hcl files
 
+echo -e "\n=> Searching for .hcl files"
 /terragrunt-fmt.sh -recursive -write=true
+
+echo -e "\n=> Searching for .tf and .tfvars files"
 terraform fmt -recursive -write=true
