145123a10a7e52698b2338c4d54854cdc1cade65
Krzysztof Szyper <krzysztof_szyper@epam.com>
2019-12-16 11:08:32 +0100

    Tables instead of lists in README.md.

--- fmt/format-hcl.sh
+++ fmt/format-hcl.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-# Formats all HCL-format files under working directory
+# Formats all HCL-format files under working directory including subdirectories
 # Runs `terraform fmt` for all .tf files
 # Run `/terragrunt-fmt.sh` for all .hcl files
+# Using cytopia's terragrunt-fmt.sh script (https://github.com/cytopia/docker-terragrunt-fmt) for .hcl files
+# Using terraform for .tf and .tfvars
+# Default action is to search recursively and modify all files.
 
 echo -e "\n=> Searching for .hcl files"
 /terragrunt-fmt.sh -recursive -write=true
